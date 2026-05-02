a3b7d5d7af04a0672f9868f006dc568eb01ab0bf
Kauan-01 <k.kantovick2008@gmail.com>
2026-05-02 19:55:33 -0300

    first commit

---
+++ S02-repo/script.js
@@ -0,0 +1,23 @@
+let idade = 27
+let peso = 180
+
+console.log(idade >= 18 && peso < 100)
+
+let soma = 5 + 4;
+let subtração = 10 - 3;
+let multiplicaçao = 6 * 2;
+let exponenciacao = 2 ** 3;
+let divisao = 20 / 4;
+let resto = 10 % 3;
+
+console.log(soma);
+console.log(subtração);
+console.log(multiplicaçao);
+console.log(exponenciacao);
+console.log(divisao);
+console.log(resto);
+
+function eMaiorDeIdade(idade) {
+    return idade <= 18
+}
+                         
